578d351dc89b66c4e81e1294e9b1fd98060b0d2f
Rhendy Saragih <rhendygio19gmail.com>
2024-09-09 20:56:17 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('donasi-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Logika untuk menambahkan proyek donasi ke dalam daftar
+  });
+  
+  document.getElementById('barang-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Logika untuk menambahkan barang bekas ke dalam daftar
+  });
+  
+  document.getElementById('forum-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Logika untuk menambahkan diskusi forum
+  });
+  
